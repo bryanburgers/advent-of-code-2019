9498e1c22d479fe7d8f6c976507c4558258a3121
Bryan Burgers <bryan@burgers.io>
2019-12-02 05:53:11 -0600

    01b

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,5 +1,13 @@
-use std::io::{self, BufRead, Read};
+use std::io::{self, BufRead};
 use std::str::FromStr;
+
+fn simple_fuel_required(mass: usize) -> usize {
+    if mass <= 8 {
+        return 0;
+    }
+
+    ((mass as f64) / 3.0).floor() as usize - 2
+}
 
 #[derive(Debug)]
 struct SpaceModule {
@@ -12,7 +20,20 @@ impl SpaceModule {
     }
 
     fn fuel_required(&self) -> usize {
-        ((self.mass as f64) / 3.0).floor() as usize - 2
+        simple_fuel_required(self.mass)
+    }
+
+    fn adjusted_fuel_required(&self) -> usize {
+        let mut total = 0;
+        let mut last = self.mass;
+        loop {
+            last = simple_fuel_required(last);
+            if last == 0 {
+                break;
+            }
+            total += last;
+        }
+        total
     }
 }
 
@@ -28,13 +49,19 @@ impl FromStr for SpaceModule {
 
 fn main() {
     let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
+    let lines = stdin.lock().lines();
 
-    let fuel_required = lines
+    let (fuel_required, adjusted_fuel_required) = lines
         .map(|line| line.unwrap().parse::<SpaceModule>().unwrap())
-        .fold(0, |sum, module| sum + module.fuel_required());
+        .fold((0, 0), |(sum, adjusted_sum), module| {
+            (
+                sum + module.fuel_required(),
+                adjusted_sum + module.adjusted_fuel_required(),
+            )
+        });
 
     println!("fuel required: {}", fuel_required);
+    println!("adjusted fuel required: {}", adjusted_fuel_required);
 }
 
 #[cfg(test)]
@@ -54,5 +81,20 @@ mod test {
 
         let sm = SpaceModule::new(100756);
         assert_eq!(sm.fuel_required(), 33583);
+    }
+
+    #[test]
+    fn test_adjusted() {
+        let sm = SpaceModule::new(12);
+        assert_eq!(sm.adjusted_fuel_required(), 2);
+
+        let sm = SpaceModule::new(14);
+        assert_eq!(sm.adjusted_fuel_required(), 2);
+
+        let sm = SpaceModule::new(1969);
+        assert_eq!(sm.adjusted_fuel_required(), 966);
+
+        let sm = SpaceModule::new(100756);
+        assert_eq!(sm.adjusted_fuel_required(), 50346);
     }
 }
