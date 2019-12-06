5ece8eb2fae2bbd1813dd57557a8b66aa6cd811e
Bryan Burgers <bryan@burgers.io>
2019-12-06 06:49:48 -0600

    06b

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -51,6 +51,56 @@ fn path_size(key: &str, map: &HashMap<String, String>) -> usize {
     }
 }
 
+fn chain(key: &str, map: &HashMap<String, String>) -> Vec<String> {
+    let mut vec = Vec::new();
+
+    let mut key = key;
+    vec.push(key.into());
+    loop {
+        if let Some(next) = map.get(key) {
+            vec.push(next.into());
+            key = next;
+        } else {
+            break;
+        }
+    }
+
+    vec
+}
+
+fn common_ancestor(v1: &[String], v2: &[String]) -> Option<String> {
+    let mut result = None;
+
+    for i in 0..v1.len() {
+        if v1[i] == v2[i] {
+            result = Some(v1[i].clone());
+        } else {
+            break;
+        }
+    }
+
+    result
+}
+
+fn jumps_between(k1: &str, k2: &str, map: &HashMap<String, String>) -> usize {
+    let mut k1_chain = chain(k1, map);
+    k1_chain.reverse();
+    let mut k2_chain = chain(k2, map);
+    k2_chain.reverse();
+
+    let common_ancestor =
+        common_ancestor(&k1_chain, &k2_chain).expect("k1 and k2 should have a common ancestor");
+
+    let common_ancestor_len = path_size(&common_ancestor, map);
+    let k1_len = k1_chain.len();
+    let k2_len = k2_chain.len();
+
+    let distance_from_k1_to_ancestor = k1_len - common_ancestor_len - 1;
+    let distance_from_k2_to_ancestor = k2_len - common_ancestor_len - 1;
+
+    distance_from_k1_to_ancestor - 1 + distance_from_k2_to_ancestor - 1
+}
+
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
@@ -62,6 +112,10 @@ fn main() {
     let checksum = checksum(&map);
 
     println!("checksum={}", checksum);
+
+    let jumps_between = jumps_between("YOU", "SAN", &map);
+
+    println!("jumps_between={}", jumps_between);
 }
 
 #[cfg(test)]
@@ -94,5 +148,65 @@ mod test {
         let map = build_map(orbits);
 
         assert_eq!(checksum(&map), 42);
+    }
+
+    #[test]
+    fn test_chain() {
+        let orbits = &[
+            "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
+        ];
+
+        let orbits = orbits
+            .into_iter()
+            .map(|listing| listing.parse::<OrbitListing>().unwrap());
+
+        let map = build_map(orbits);
+
+        assert_eq!(chain("J", &map), vec!["J", "E", "D", "C", "B", "COM"]);
+    }
+
+    #[test]
+    fn test_common_ancestor() {
+        let v1 = vec![
+            "0".into(),
+            "1".into(),
+            "2".into(),
+            "3".into(),
+            "4".into(),
+            "5".into(),
+            "6".into(),
+            "7".into(),
+            "8".into(),
+            "9".into(),
+            "10".into(),
+        ];
+        let v2 = vec![
+            "0".into(),
+            "1".into(),
+            "2".into(),
+            "3".into(),
+            "4".into(),
+            "11".into(),
+            "12".into(),
+            "13".into(),
+        ];
+
+        assert_eq!(common_ancestor(&v1[..], &v2[..]), Some("4".into()));
+    }
+
+    #[test]
+    fn test_jumps_between() {
+        let orbits = &[
+            "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU",
+            "I)SAN",
+        ];
+
+        let orbits = orbits
+            .into_iter()
+            .map(|listing| listing.parse::<OrbitListing>().unwrap());
+
+        let map = build_map(orbits);
+
+        assert_eq!(jumps_between("YOU", "SAN", &map), 4);
     }
 }
